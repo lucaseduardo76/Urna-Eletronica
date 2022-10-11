f4a70eea0047e5f7fe5100e6c29befdffabe9c80
Lucas Eduardo <lesds2001@gmail.com>
2022-10-10 22:55:10 -0300

    Adicionando itens

---
+++ assets/script/etapas.js
@@ -0,0 +1,125 @@
+let etapas = [
+    {
+        titulo: 'DEPUTADO ESTADUAL',
+        numeros: 4,
+        candidatos:[
+            {
+                numero: '2001',
+                nome: 'Valntina',
+                partido: 'partido dos Bebês',
+                fotos:[
+                    {url: 'imagens/valentina.jpg', legenda: 'Deputado Estadual'}
+                ]
+            },
+            {
+                numero: '1911',
+                nome: 'Isadora',
+                partido: 'partido dos Bebês',
+                fotos:[
+                    {url: 'imagens/isadora.jpg', legenda: 'Deputado Estadual'}
+                ]
+            },
+            {
+                numero: '1605',
+                nome: 'Laura',
+                partido: 'partido dos Bebês',
+                fotos:[
+                    {url: 'imagens/lauriete.jpg', legenda: 'Deputado Estadual'}
+                ]
+            },
+            {
+                numero: '2704',
+                nome: 'Ana Laura',
+                partido: 'partido dos Bebês',
+                fotos:[
+                    {url: 'imagens/ana.jpg', legenda: 'Deputado Estadual'}
+                ]
+            },
+
+        ]
+    },
+    {
+        titulo: 'DEPUTADO FEDERAL',
+        numeros: 4,
+        candidatos:[
+            {
+                numero: '2009',
+                nome: 'Larah Raquel',
+                partido: 'Partido dos Adolecentes',
+                fotos:[
+                    {url: 'imagens/larah.jpg', legenda: 'Deputado Federal'}
+                ]
+            },
+            {
+                numero: '3106',
+                nome: 'Alexsander',
+                partido: 'partido dos Adolecentes',
+                fotos:[
+                    {url: 'imagens/alexsander.jpg', legenda: 'Deputado Federal'}
+                ]
+            },
+            {
+                numero: '2611',
+                nome: 'Inácio',
+                partido: 'partido dos Adolecentes',
+                fotos:[
+                    {url: 'imagens/inacio.jpg', legenda: 'Deputado Federal'}
+                ]
+            },
+            {
+                numero: '1566',
+                nome: 'Raquel',
+                partido: 'partido dos Adolecentes',
+                fotos:[
+                    {url: 'imagens/raquel.jpg', legenda: 'Deputado Federal'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'Presidente',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: '29',
+                nome: 'Lucas Eduardo',
+                partido: 'Partido The Mudelos',
+                fotos:[
+                    {url: 'imagens/lucas.jpg', legenda: 'Presidente da Republica'}
+                ]
+            },
+            {
+                numero: '31',
+                nome: 'Áilla Almeida',
+                partido: 'Partido The Mudelos',
+                fotos:[
+                    {url: 'imagens/ailla.jpg', legenda: 'Presidente da Republica'}
+                ]
+            },
+            {
+                numero: '28',
+                nome: 'Nathalia Raquel',
+                partido: 'Partido The Mudelos',
+                fotos:[
+                    {url: 'imagens/nathalia.jpg', legenda: 'Presidente da Republica'}
+                ]
+            },
+            {
+                numero: '31',
+                nome: 'Nathan Fellipe',
+                partido: 'Partido The Mudelos',
+                fotos:[
+                    {url: 'imagens/nathan.jpg', legenda: 'Presidente da Republica'}
+                ]
+            },
+            {
+                numero: '29',
+                nome: 'Wilke Meireles',
+                partido: 'Partido The Mudelos',
+                fotos:[
+                    {url: 'imagens/wilke.jpg', legenda: 'Presidente da Republica'}
+                ]
+            }
+        ]
+    }
+];
